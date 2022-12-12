ed7d5e91b196c35acc60dd1c600423eb46bdeef1
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2022-12-13 01:17:15 +0200

    Create question.js

---
+++ container/question.js
@@ -0,0 +1,13 @@
+
+export  function Question(text, choices, answer) {
+   
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;};
+  
+    Question.prototype.correctAnswer=function(choices) {
+        return choices === this.answer;
+    };
+
+
+
